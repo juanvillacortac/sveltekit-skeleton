7f8a384ffe2aa0ddfa119d45a99f9c7260be0ffc
Juan Villacorta <juanvillacortac@gmail.com>
2023-04-14 14:10:40 -0400

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { browser } from "$app/environment";
+
+  import "virtual:windi.css";
+  import "virtual:windi-devtools";
+
+  // if you want to enable windi devtools
+  // @ts-ignore
+  if (browser) import("virtual:windi-devtools");
+</script>
+
+<slot />
---
+++ windicss.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "windicss/helpers";
+import formsPlugin from "windicss/plugin/forms";
+
+export default defineConfig({
+  theme: {
+    extend: {
+      colors: {},
+    },
+  },
+  plugins: [formsPlugin],
+});
